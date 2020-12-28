0cf9f2a07648cc82ebb92ddb8eabb4ee19ec5fec
Techm92 <74015762+Techm92@users.noreply.github.com>
2020-12-27 22:53:55 -0500

    Update countOfAllBooleans.js

--- 04-countOfAllBooleans/countOfAllBooleans.js
+++ 04-countOfAllBooleans/countOfAllBooleans.js
@@ -1,5 +1,15 @@
 function countOfAllBooleans(arr) {
   // Insert code here;
+var boolCount = 0;
+
+for (var i = 0; i < arr.length; i++)
+{
+    if (arr[i] === true || arr[i] === false)
+    {
+        boolCount = boolCount + 1;
+    }
+}
+ document.write(boolCount);
 }
 
 // Do not edit this line;
