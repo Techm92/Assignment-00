10d27b1c4d51447c680ee8329638eccf4fdd8eaa
Techm92 <74015762+Techm92@users.noreply.github.com>
2020-12-27 22:58:20 -0500

    Update sumOfNumsWithinARange.js

--- 10-sumOfNumsWithinARange/sumOfNumsWithinARange.js
+++ 10-sumOfNumsWithinARange/sumOfNumsWithinARange.js
@@ -1,5 +1,16 @@
 function sumOfNumsWithinARange(nums, start, end) {
   // Insert code here;
+  var rangeCount = 0;
+
+  for(var i = 0; i < nums.length; i++)
+  {
+    if(nums[i] >= start && nums[i] <= end)
+    {
+      rangeCount += 1;
+    }
+
+  }
+  document.write(rangeCount);
 }
 
 // Do not edit this line;
