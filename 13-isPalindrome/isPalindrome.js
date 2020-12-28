83d7a9aee2969e69df4f8972509ba1c6f959f381
Techm92 <74015762+Techm92@users.noreply.github.com>
2020-12-27 23:00:18 -0500

    Update isPalindrome.js

--- 13-isPalindrome/isPalindrome.js
+++ 13-isPalindrome/isPalindrome.js
@@ -1,5 +1,21 @@
 function isPalindrome(word) {
   // Insert code here;
+  var Reverse_Word = '';
+  
+  for( var i = word.length - 1 ; i >= 0 ; i-- )
+  {
+    Reverse_Word += word.charAt(i);
+    //document.write(Reverse_Word);
+  }
+  
+  //document.write(Reverse_Word);
+
+  if ( word === Reverse_Word)
+  document.write(true,"<br />");
+
+  else {
+  document.write(false, "<br />");
+  }
 }
 
 // Do not edit this line;
