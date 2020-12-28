672e1dc55056192e9b756aab388add202a5d2097
Techm92 <74015762+Techm92@users.noreply.github.com>
2020-12-27 23:09:18 -0500

    Update pairSum.js

--- 18-pairSum/pairSum.js
+++ 18-pairSum/pairSum.js
@@ -1,5 +1,25 @@
 function pairSum(nums, target) {
   // Insert code here;
+    var intLeft = 0,
+        arrIntegersLength = nums.length;
+        intRight = arrIntegersLength - 1,
+        //arrReturn = [];
+
+    nums.sort();
+
+    while (intLeft < intRight) {
+        var currentSum = nums[intLeft] + nums[intRight];
+        if (currentSum === target) {
+          return true;
+            //arrReturn.push(nums[intLeft] + " + " + nums[intRight]);
+            intLeft += 1;
+        } else if ( currentSum < target) {
+            intLeft += 1;
+        } else {
+            intRight -= 1;
+        }
+    }
+    return false;
 }
 
 // Do not edit this line;
