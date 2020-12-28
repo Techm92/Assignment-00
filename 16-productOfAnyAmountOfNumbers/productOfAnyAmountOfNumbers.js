840d3d4478926b3c3aadee624fa305e98f3efa95
Techm92 <74015762+Techm92@users.noreply.github.com>
2020-12-27 23:01:42 -0500

    Update productOfAnyAmountOfNumbers.js

--- 16-productOfAnyAmountOfNumbers/productOfAnyAmountOfNumbers.js
+++ 16-productOfAnyAmountOfNumbers/productOfAnyAmountOfNumbers.js
@@ -1,5 +1,14 @@
 function productOfAnyAmountOfNumbers(...args) {
   // Insert code here;
+  var product = args[0];
+
+  for (var i = 1; i < args.length; i++){
+    //document.write(args[i],"<br />");
+    //document.write(product,"<br />");
+    product *= args[i];
+    //document.write(product,"<br />");
+  }
+    document.write(product,"<br />");
 }
 
 // Do not edit this line;
