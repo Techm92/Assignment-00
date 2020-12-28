248586ef7ba143c2d10682fb7e3eb9b2c7332d18
Techm92 <74015762+Techm92@users.noreply.github.com>
2020-12-27 22:59:41 -0500

    Update fizzBuzz.js

--- 12-fizzBuzz/fizzBuzz.js
+++ 12-fizzBuzz/fizzBuzz.js
@@ -1,5 +1,27 @@
 function fizzBuzz(start, end) {
   // Insert code here;
+  for(var i = start; i <= end; i++){
+
+    if (i % 3 == 0 && i % 5 == 0)
+    {
+      document.write("FizzBuzz");
+    }
+
+    else if (i % 3 == 0)
+    {
+      document.write("Fizz");
+    }
+
+    else if (i % 5 == 0)
+    {
+            document.write("Buzz");
+    }
+
+    else
+       // output.push(i);
+       document.write(i);
+  }
+  document.write("<br />");
 }
 
 // Do not edit this line;
