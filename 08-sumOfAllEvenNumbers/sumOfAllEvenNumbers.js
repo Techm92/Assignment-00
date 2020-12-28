286d4ad6abb4c55034bf245f051274686662bfdf
Techm92 <74015762+Techm92@users.noreply.github.com>
2020-12-27 22:57:48 -0500

    Update sumOfAllEvenNumbers.js

--- 08-sumOfAllEvenNumbers/sumOfAllEvenNumbers.js
+++ 08-sumOfAllEvenNumbers/sumOfAllEvenNumbers.js
@@ -1,5 +1,15 @@
 function sumOfAllEvenNumbers(nums) {
   // Insert code here;
+  var evenCount= 0;
+
+  for( var i =0; i < nums.length; i++)
+  {
+    if(nums[i] % 2 == 0)
+    {
+      evenCount += 1;
+    }
+  }
+  document.write(evenCount);
 }
 
 // Do not edit this line;
