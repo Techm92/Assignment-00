fae6ce334197704d480717df5b756d2c310e44b2
Techm92 <74015762+Techm92@users.noreply.github.com>
2020-12-27 22:54:50 -0500

    Update countOfAllNumbersSmallerThanTarget.js

--- 06-countOfAllNumbersSmallerThanTarget/countOfAllNumbersSmallerThanTarget.js
+++ 06-countOfAllNumbersSmallerThanTarget/countOfAllNumbersSmallerThanTarget.js
@@ -1,5 +1,16 @@
 function countOfAllNumbersSmallerThanTarget(nums, target) {
   // Insert code here;
+    var numCount = 0;
+
+    for(var i = 0; i < nums.length; i++)
+    {
+      if( nums[i] < target)
+      {
+        numCount += 1;
+      }
+
+    }
+    document.write(numCount);
 }
 
 // Do not edit this line;
