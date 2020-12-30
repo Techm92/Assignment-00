433f4ad91120fe0725aa4ee6e15f5ca29fe7b217
Techm92 <74015762+Techm92@users.noreply.github.com>
2020-12-30 16:05:27 -0500

    Update setUnionOfAnyAmountOfSets.js

--- 17-setUnionOfAnyAmountOfSets/setUnionOfAnyAmountOfSets.js
+++ 17-setUnionOfAnyAmountOfSets/setUnionOfAnyAmountOfSets.js
@@ -1,6 +1,14 @@
 function setUnionOfAnyAmountOfSets(...args) {
   // Insert code here;
-  // Insert code here;
+  var newSet = [];
+
+  for (var i = 0; i < arguments.length; i++) {
+    if( arguments[i] != arguments[i+1] )
+    {
+      document.write(arguments[i]);
+      newSet = newSet.concat(arguments[i]);
+    }
+  }
 }
 
 // Do not edit this line;
