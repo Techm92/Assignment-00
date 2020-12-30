e1d02acc584ab0df649093fa0ce7389051f28301
Techm92 <74015762+Techm92@users.noreply.github.com>
2020-12-30 15:23:05 -0500

    Update frequencyCounter.js

--- 14-frequencyCounter/frequencyCounter.js
+++ 14-frequencyCounter/frequencyCounter.js
@@ -1,5 +1,20 @@
 function frequencyCounter(word) {
   // Insert code here;
+ var letter_Count1 = 1;
+ var letter_Count = 1;
+ for (var position = 0; position < word.length; position++) 
+ {
+    if (word.charAt(position) == word.charAt(position+1)) 
+      {
+      letter_Count1 += 1;
+      document.write(" ", word.charAt(position), " : ", letter_Count1, " ");
+      }
+      else if(word.charAt(position) != word.charAt(position+1)){
+      letter_Count == 1;
+      document.write(" ", word.charAt(position), " : ", letter_Count, " ");
+      }
+  }
+  //document.write("<br />");
 }
 
 // Do not edit this line;
